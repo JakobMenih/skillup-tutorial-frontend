9bf47e9267415c513f1c7ba7ce50d419e6bf4797
JakobMenih <jakob.menih@gmail.com>
2024-05-20 17:09:05 +0200

    3 Tutorial

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
 import Layout from '../components/ui/Layout'
+import LoginForm from '../components/user/LoginForm'
 
 const Login: FC = () => {
   return (
     <Layout>
-      {/* <LoginForm/> */}
-      Login
+      <LoginForm />
     </Layout>
   )
 }
